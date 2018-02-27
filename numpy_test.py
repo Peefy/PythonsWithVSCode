98756b5724b2b6f74062a4a556ef117bfe1fcea7
Peefy <xpf6677@163.com>
2018-02-27 10:50:30 +0800

    Add np.matrix test
    Add np.matrix test

--- numpy_test.py
+++ numpy_test.py
@@ -1,13 +1,31 @@
 # python3 numpy_test.py
 # python numpy_test.py
-import numpy as nm
-X = nm.array([2,3,45,1,23])
-print(nm.sort(X))
+import numpy as np
+from numpy.random import rand
+from numpy import *
+
+X = np.array([2,3,45,1,23])
+print(np.sort(X))
 print([x ** 2 for x in X if x < 30 and x > 10]) 
-a = nm.array((1,2,3))
+a = np.array((1,2,3))
 print(a)
 print('')
+A = matrix(zeros((3, 3)))
+# matlab索引从1开始，python从0开始
+A[1, 1] = 3
+B = matrix(np.random.rand(3, 3))
+B[2, 2] = 5
+print(A)
+print(B)
+# 矩阵转置
+print(B.T)
+print(A + B)
+print(A - B)
+# 矩阵相乘
+print(A * B)
+# 矩阵点乘
+print(multiply(A, B))
 
 
-#python numpy_test.py
-#python3 numpy_test.py
+# python numpy_test.py
+# python3 numpy_test.py
