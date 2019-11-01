f91f07e16a4cf467d224adb4f1add0e28f69510e
Peefy <xpf6677@163.com>
2019-11-01 21:42:11 +0800

    Update

---
+++ yougetbilibili.py
@@ -0,0 +1,9 @@
+
+import sys
+from you_get import common as you_get       
+
+directory = r'E:\xpf\Video'                       
+url = 'https://www.bilibili.com/video/av11570261?from=search&seid=7224614101954159775'     
+sys.argv = ['you-get','-o',directory,url]      
+you_get.main()
+
