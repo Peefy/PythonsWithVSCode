dd12907874d96d878728bae8f8d13c65c9eb3242
Peefy <xpf6677@163.com>
2020-02-26 21:46:07 +0800

    Update nowcoder2.py

--- src/nowcoder2.py
+++ src/nowcoder2.py
@@ -1,20 +1,20 @@
-
 import sys
-
 IS_TEST_DATA = False
-
 if IS_TEST_DATA == True:
-    datas = '[1 1 3 3 2 2]'
+    datas = '[2.2 1]'
 else:
     datas = sys.stdin.readline()
-
-datas = datas[1:len(datas) - 1]
-nums = datas.split()
-for i in range(len(nums)):
-    nums[i] = int(nums[i])
-snums = sorted(set(nums))
-for snum in snums:
-    print(snum, end=' ')
+if datas is not None and len(datas) > 2:
+    datas = datas[1:len(datas) - 1]
+    nums = datas.split()
+    for i in range(len(nums)):
+        if '.' not in nums[i]:
+            nums[i] = int(nums[i])
+        else:
+            nums[i] = float(nums[i])
+    snums = list(sorted(list(set(nums))))
+    for snum in snums:
+        print(snum, end=' ')
 
 
 
