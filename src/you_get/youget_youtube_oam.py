afa9f97d62f88606d78cfc6a28953ac64ef10cf1
Peefy <xpf6677@163.com>
2020-05-08 14:17:00 +0800

    Remove you-get

--- src/you_get/youget_youtube_oam.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-from you_get import common as you_get       
-
-directory = r'./video/'                       
-url = 'https://www.youtube.com/watch?v=QHYJ4VpqAvs'     
-sys.argv = ['you-get','-o',directory,url]      
-you_get.main()
-
